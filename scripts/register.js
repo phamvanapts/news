ab1f0e0aa40938d693bb995cc0d6585432020d02
phamvana <72136536+phamvana@users.noreply.github.com>
2024-10-07 16:27:09 +0700

    cập nhật register, validate

--- scripts/register.js
+++ scripts/register.js
@@ -18,6 +18,7 @@ btnsubmit.addEventListener('click',function(){
      * 1. Lấy dữ liệu từ form nhập
      * gán cho các thuộc tính của user
      */
+    
     const user = new User(
         inputfirstname.value,   //giá trị nhập vào inputfirstname
         inputlastname.value,    // giá trị nhập vào inputlastname
@@ -27,16 +28,85 @@ btnsubmit.addEventListener('click',function(){
     /**
      * 2. Gọi hàm validate để kiểm tra form hợp lệ
      */
-    /**
-     * 3. Khởi tạo user mới với các dữ liệu hợp lệ
-     */
-    /**
-     * 4. Thêm user vào mảng, lưu mảng vào localStorage
-     */
-    /**
-     * 5. Chuyển trang đến màn hình login
-     */
-    alert(`${user.inputpassword.value}`);
+    const isValidate = validate(user);
+    if(isValidate){
+        /**
+         * 3. Khởi tạo user mới với các dữ liệu hợp lệ
+        */
+       
+        userArr.push(user);
+       console.log(userArr);
+       
+        /**
+         * 4. Thêm user vào mảng, lưu mảng vào localStorage
+        */
+       saveToStorage("userArr", userArr);
+        /**
+         * 5. Chuyển trang đến màn hình login
+         */
+        window.location.assign('../pages/login.html');
+    }else{
+        alert("Không đăng ký được thành viên!")
+    }
+    
 } //finish function
 ) //finish button Submit
-// alert(inputfirstname.value);
+/**
+ * Hàm kiểm tra nhập liệu của người dùng 
+ */
+function checkInput(user){ 
+    if(inputfirstname.value == ""){ //Kiểm tra trường firstname có rỗng không.
+        alert(" Please, Input First Name !"); //Thông báo phải nhập First name
+        return false;  // Trả về giá trị false ==> Không ghi được dữ liệu
+    }else if(inputlastname.value ==""){
+        alert(" Please, Input Last Name !"); //Thông báo phải nhập LastName
+        return false;  // Trả về giá trị false ==> Không ghi được dữ liệu
+    }else if(inputusername.value ==""){
+        alert(" Please, Input User Name !"); //Thông báo phải nhập LastName
+        return false;  // Trả về giá trị false ==> Không ghi được dữ liệu
+    }else if(inputpassword.value ==""){
+        alert(" Please, Input PassWord !"); //Thông báo phải nhập LastName
+        return false;  // Trả về giá trị false ==> Không ghi được dữ liệu
+    }else if(inputpasswordconfirm.value ==""){
+        alert(" Please, Input Confirm PassWord !"); //Thông báo phải nhập LastName
+        return false;  // Trả về giá trị false ==> Không ghi được dữ liệu
+    }else{
+        return true;    // Trả về true ==> kiểm tra các điều kiện khác.
+    }
+    // alert(" Có dữ liệu ID !");
+}
+/**
+ * username không được trùng với username của người dùng khác trước đó
+ * @param {*} user 
+ */
+function userNameUnique(user){
+    /**
+     * Nếu mảng userArr rỗng thì không cần kiểm tra ==> true
+     */
+    if(userArr.length == 0) true; 
+    // alert(`userArr.length = ${userArr.length}`);
+    for(let i = 0; i < userArr.length; i++){ // lập hết mảng petArr
+        if(userArr[i].userName == inputusername.value){             // Nếu petArr thứ i trùng với giá trị kiểm tra
+            alert(` Trùng = ${inputusername.value} !`);      // Thông báo trùng ID
+            return false;                   // trả về false ==> Không thực hiện ghi dữ liệu.    
+        }
+    }   // kết thúc for
+    return true;
+}
+/**
+ * Hàm kiểm tra giá trị nhập đăng ký thành viên mới
+ * @param {*} user mảng user nhập vào để kiểm tra
+ * Không có trường nào bị bỏ trống. ==> ok
+ * username không được trùng với username của người dùng khác trước đó. ==> ok
+ * Password và confirm password phải giống nhau. 
+ * Password có nhiều hơn 8 ký tự.
+ */
+function validate(user){
+    if(    checkInput(user)
+        && userNameUnique(user)){
+        // alert(`kiểm tra validate - input ${checkInput(user)}`);
+        return true;
+    }else{
+        return false; //tạo giá trị true để kiểm tra
+    }
+}
--- scripts/storage.js
+++ scripts/storage.js
@@ -1,9 +1,37 @@
 'use strict'
 // alert("Test Storage.js");
-
+/**
+ * Lưu giá trị key, value vào localStorage
+ * @param {*} key 
+ * @param {*} value
+ * Lấy giá trị key và value lưu ở dạng JSON
+ */
 const saveToStorage = function (key, value){
     localStorage.setItem(key, JSON.stringify(value));
 };
+/**
+ * Lấy giá trị key đểgans cho một mảng cần thiết
+ * @param {*} key 
+ * @returns  trả về JSON bằng hàm parse
+ */
 const getFromStorage = function(key){
 return JSON.parse(localStorage.getItem(key));
 };
+/**
+ * Tạo và gán biến users là mảng lấy từ localStorage hoặc rổng
+ */
+const users = getFromStorage('userArr')? getFromStorage('userArr') : [];
+/**
+ * Chuyển đổi về dạng Class Intance
+ * Sẽ trả về một mảng chứa các intance của Class User
+ */
+const userArr = users.map((user)=>parseUser(user));
+function parseUser(userData){
+    const user = new User(
+        userData.firstName,
+        userData.lastName,
+        userData.userName,
+        userData.passWord,
+    );
+    return user;
+}
